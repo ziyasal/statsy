46506cfd4acadbb5ff9ca75387ea7ef98cb93d6c
O(z) <sarikayaziya@gmail.com>
2018-06-15 15:03:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,17 @@
 
 const _ = require('lodash');
 const StatsCollector = require('./stats-collector');
-const constants = require('./constants');
-const makeRandomArray = require('./random-array-maker');
+
+const MAX_RESPONSE_TIME_IN_MS = 19000;
+const MIN_RESPONSE_TIME_IN_MS = 15;
+const SEED_DATA_SIZE = 100000;
 
 function main() {
     let statsCalculator = new StatsCollector();
-    var sampleResponseTimes = makeRandomArray(15, constants.MAX_RESPONSE_TIME, 100000);
-
-    sampleResponseTimes.forEach(item => {
-        statsCalculator.pushValue(item);
-    });
+    
+    for (var i = 0; i < SEED_DATA_SIZE; i++) {
+       statsCalculator.pushValue(_.random(MIN_RESPONSE_TIME_IN_MS, MAX_RESPONSE_TIME_IN_MS))
+    }
 
     let median = statsCalculator.getMedian();
     console.log('MEDIAN %s', median);
